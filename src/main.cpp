71d21ecd0410bb9955fea42a72e492ffd26d532b
Mai Beckers <mai@git.local>
2024-06-24 12:10:06 +0200

    first commit neu

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 
 void setup () {
-// Initialisiert Pin 14 (LED1) aals output.
+// Initialisiert Pin 14 (LED1) als output.
 pinMode (14 , OUTPUT) ;
 }
 // LED1 blinkt im Zweisekundentakt
@@ -9,5 +9,5 @@ void loop () {
 digitalWrite (14 , HIGH ) ; // LED1 an
 delay (1000) ; // 1000 ms warten
 digitalWrite (14 , LOW) ; // LED1 aus
-delay (1000) ; // 1000 ms war ten
+delay (1000) ; // 1000 ms warten
 }
